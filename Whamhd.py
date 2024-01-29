fe9c1c66f39e57b08eb5d7ec112eab1ec632a3c1
Tony Qian <tqian@pppl.gov>
2024-01-29 01:44:02 -0600

    Adding synthetic probe diagnostic
    Implementing spectral analysis and plots

--- Whamhd.py
+++ Whamhd.py
@@ -1,15 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
-from matplotlib.pylab import cm as cmap
+from matplotlib.pylab import cm as colormap
+
+from scipy.interpolate import RegularGridInterpolator
+from scipy.fft import fft, ifft, fftfreq, fftshift
 
 # user def
 path = "/Users/tqian/Documents/WHAM/Wham-Theory/simulation/gkeyll/2023-1005-gkyl-data"
 
 # constants
-A_probe = 9.5e-6 # m2
 mass_ratio = 1840
-pi = np.pi
 e = 1.6e-19
 Mi = 1.67e-27
 
@@ -44,7 +45,6 @@ class SyntheticDiagnostic:
         The function interperloates data on a recular grid (T,Z,Y,X)
         '''
         
-        from scipy.interpolate import RegularGridInterpolator
         gkyl = self
         grid = (gkyl.t, gkyl.z, gkyl.y, gkyl.x)
         f_Ne = RegularGridInterpolator(grid, gkyl.Ne)
@@ -80,6 +80,18 @@ class SyntheticDiagnostic:
         self.probes = np.transpose([z,y,x])
         self.M = M
         self.z_idx = z_idx
+        self.R_probes = R
+
+    def sampleProbes(self,area=1e-6):
+        '''
+        probe area in m^2
+        '''
+        ne = self.sample_Ne
+        Te = self.sample_Te
+        phi = self.sample_Phi
+
+        self.Isat = 0.6*e*ne*np.sqrt(Te/Mi) * area
+        self.Vf = phi + (Te/e) * np.log(0.6*np.sqrt(2*np.pi/mass_ratio))
 
     def GridPlot(self, t, fig=None):
         if fig==None:
@@ -89,10 +101,10 @@ class SyntheticDiagnostic:
         z = self.z_idx
         probes = self.probes
 
-        gs = gridspec.GridSpec(M, 2, figure=fig)
-        ax0 = fig.add_subplot(gs[:,0])
         T = gkyl.t[t]
         Z = gkyl.z[z]
+        gs = gridspec.GridSpec(M, 2, figure=fig)
+        ax0 = fig.add_subplot(gs[:,0])
         ax0.contourf(gkyl.x, gkyl.y, gkyl.Ne[t,z],20,cmap='inferno')
         ax0.set_title(f"Ne: t = {T:.3e},  Z = {Z:.2f}")
         for j in np.arange(M):
@@ -106,3 +118,104 @@ class SyntheticDiagnostic:
         
         fig.suptitle(f"frame {t} : t = {T:.3e}")
         fig.tight_layout()
+
+    def ProbePlot(self,t,cmap,fig=None):
+        if fig==None:
+            fig = plt.figure(figsize=(10,5))
+        M = self.M
+        gkyl = self
+        z = self.z_idx
+        probes = self.probes
+
+        T = gkyl.t[t]
+        Z = gkyl.z[z]
+        t_axis = gkyl.t * 1e3
+        gs = gridspec.GridSpec(M, 3, figure=fig)
+    
+        ax0 = fig.add_subplot(gs[:,0])
+        ax0.contourf(gkyl.x*100, gkyl.y*100, gkyl.Ne[t,z],20,cmap='inferno')
+        ax0.set_title(f"Ne: t = {T:.3e},  Z = {Z:.2f}")
+        ax0.set_xlabel("X [cm]")
+        ax0.set_ylabel("Y [cm]")
+    
+        k_grid = M // 2
+        ax_d1 = fig.add_subplot(gs[:k_grid,2])
+        ax_d2 = fig.add_subplot(gs[k_grid:,2])
+    
+        for j in np.arange(M):
+            ax = fig.add_subplot(gs[j,1])
+            ax.plot(t_axis, self.sample_Ne[j], 'C1')
+            ax.axvline(T*1e3, ls='--', color=cmap[j])
+            ax.set_title("X = {:.2f}, Y = {:.2f}, Z = {:.2f}".format(*probes[j][::-1]))
+        
+            _,py,px = probes[j] * 100
+            ax0.plot(px,py,'o', color=cmap[j])
+    
+            ax_d1.plot(t_axis, self.Vf[j], color=cmap[j])
+            ax_d2.plot(t_axis, self.Isat[j]*1e3, color=cmap[j])
+    
+        ax_d1.set_title("Floating Potential [V]")
+        ax_d2.set_title("Ion Saturation [mA]")
+    
+        ax.set_xlabel("time [ms]")
+        ax_d2.set_xlabel("time [ms]")
+        ax_d1.axvline(T*1e3, ls='--', color='C1')
+        ax_d2.axvline(T*1e3, ls='--', color='C1')
+    
+        ax_d1.grid()
+        ax_d2.grid()
+        ax0.set_aspect('equal')
+    
+        fig.suptitle(f"frame {t} : t = {T:.3e}")
+        fig.tight_layout()
+
+    def spectralAnalysis(self,signals):
+
+        M = len(signals)
+        R = self.R_probes
+        ds = np.pi*2*R/M
+        self.m_wave = fftfreq(M,ds) * (2*np.pi*R)
+        self.kspec = np.transpose( [fft(s) for s in signals.T] )
+
+
+    def plot_kTime(self,signals):
+        '''
+        plot the magnitude and phase components of spatial FT as function of time
+        '''
+        time = self.t * 1e3
+        warm = colormap.autumn(np.linspace(0, 1, M))
+        fig,axs = plt.subplots(3,1, figsize=(14,6))
+        for j in np.arange(M):
+            if j==0:
+                axs[0].plot(time, np.abs(kspec[j]), label=f"m = {m_wave[j]}", color=warm[j])
+            else:
+                axs[1].plot(time, np.abs(kspec[j]), label=f"m = {m_wave[j]:.1f}", color=warm[j])
+            axs[2].plot(time, np.angle(kspec[j]), label=f"m = {m_wave[j]:.1f}", color=warm[j])
+        axs[0].set_ylabel('magnitude')
+        axs[1].set_ylabel('magnitude')
+        axs[2].set_ylabel('phase')
+        axs[-1].set_xlabel('time [ms]')
+        [a.legend(fontsize=8) for a in axs[:-1]]
+        fig.tight_layout()
+        
+        
+    def contour_mt(self,signals, ax=None):
+        '''
+        contour plot of wavenumber evolving in time
+        '''
+        if ax == None:
+            fig,ax = plt.subplots(1,1)
+        M = len(signals)
+        R = self.R_probes
+        ds = np.pi*2*R/M
+        time = self.t * 1e3
+        m_wave = fftfreq(M,ds) * (2*np.pi*R)
+        warm = colormap.autumn(np.linspace(0, 1, M))
+
+        dc = np.mean(signals,axis=0)
+        kspec = np.transpose( [fft(s) for s in (signals-dc).T] )
+        ax.contourf(time,fftshift(m_wave),fftshift( np.abs(kspec),axes=0),25, cmap='hot')
+        ax.set_ylabel("wave number")
+        ax.set_xlabel('time [ms]')
+
+        
