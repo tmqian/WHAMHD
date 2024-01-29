aafebe96a041ccb28bdb4b08806f8349972b662b
Tony Qian <tqian@pppl.gov>
2024-01-29 10:51:09 -0600

    Uploading test driver to make probe Spectra plot. Separate data download required.

---
+++ test_probeSpectra.py
@@ -0,0 +1,43 @@
+from Whamhd import SyntheticDiagnostic
+import numpy as np
+import matplotlib.pyplot as plt
+
+# output single frame or 1000
+video = False
+
+# user defined path
+path = "/Users/tqian/Documents/WHAM/Wham-Theory/simulation/gkeyll/2023-1005-gkyl-data"
+fin = f"{path}/32.npz"
+
+# load data from file
+data = SyntheticDiagnostic()
+data.loadGkylPack(fin)
+
+# set up probe locations
+M = 12
+z_idx = 10
+data.initProbeCircle(z_idx,M=M)
+
+# sample data at probe locations
+data.interpolate()
+A_probe = 9.5e-6 # m2
+data.sampleProbes(area=A_probe)
+
+
+# output
+if video:
+    # plot many frames to make a video, make sure outdir exists
+    outdir = "spec_01" 
+    N = len(data.t)
+    fig = plt.figure(figsize=(16,7))
+    for t in np.arange(N)[::2]:
+        data.probeSpectralAnalysis(t,fig=fig)
+        plt.savefig(f"{outdir}/{t:04d}.png")
+        fig.clear()
+    # use this line from terminal to turn set of PNG into GIF
+    ### convert -delay 5 -loop 1 *.png gk32_ne-fast.gif
+
+else:
+    # plot single frame at t=500
+    data.probeSpectralAnalysis(500)
+    plt.show()
