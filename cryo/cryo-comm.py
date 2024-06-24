84ef01aee169546ad44f5e801779414886bb6455
Tony Qian <tqian@pppl.gov>
2024-06-24 15:00:33 -0500

    minor edits, adding print messages to send/recv communications

--- cryo/cryo-comm.py
+++ cryo/cryo-comm.py
@@ -60,10 +60,12 @@ class CryoComm:
         dev = usb.core.find(idVendor=0x04d8, idProduct=0xf420)
         stat = [0x06,0x55,0xaa,0x00,0x00,0xe5,0xb5,0xff]
 
+        print(f"Sending 7 byte GET STATUS request...")
         x = dev.write(0x01,stat)
         y = dev.read(0x81,64)
 
         self.data = y
+        print(f"Success! Received {y[0]} bytes")
 
 
     def parse(self, skip=6):
