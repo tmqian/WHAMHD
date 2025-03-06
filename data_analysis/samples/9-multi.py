84f3cf3fcf4dc4a5066718e636e32ef8887503bf
Tony Qian <q@jack.>
2025-03-05 22:30:57 -0600

    updating 9-multi.py to read an optional shotlog file

--- data_analysis/samples/9-multi.py
+++ data_analysis/samples/9-multi.py
@@ -1,7 +1,16 @@
-from multi import plot9, toBool
-import MDSplus as mds
+from multi import plot9, readLog
 
 import sys
 shot = int(sys.argv[1])
-plot9(shot)
+#plot9(shot)
+
+try:
+    log = readLog(sys.argv[2])
+    entry = log[shot]
+    tag = f"{shot} : {entry}"
+    plot9(shot, tag = tag)
+    print(f"{shot} log detected")
+
+except:
+    plot9(shot)
 
